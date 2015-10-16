7b85fcecb9fb9018e479f52ac7b5303912690f43
kvncaldwll <kvn.caldwll@gmail.com>
2015-10-16 15:04:07 -0700

    tested trip with waypoints

--- tripplannertests.py
+++ tripplannertests.py
@@ -22,35 +22,36 @@ class FlaskrTestCase(unittest.TestCase):
     # MyObject tests
 
     def test_posting_trip(self):
-        response = self.app.post('/trips/', data=json.dumps(dict(trip="murica")), content_type='application/json')
+        # import pdb; pdb.set_trace()
+        my_trip = [{'trip': 'caribbean'}, {'waypoints': ['puerto rico', 'dominican republic', 'jamaica']}]
+        response = self.app.post('/trips/', data=json.dumps(dict(trip=my_trip)), content_type='application/json')
         post_responseJSON = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
         assert 'application/json' in response.content_type
-        assert 'murica' in post_responseJSON["trip"]
 
     def test_updating_trip(self):
         # post trip and confirm post success
-        response = self.app.post('/trips/', data=json.dumps(dict(trip="neverland")), content_type='application/json')
+        my_trip = [{'trip': 'africa'}, {'waypoints': ['ethiopia', 'egypt', 'south africa']}]
+        response = self.app.post('/trips/', data=json.dumps(dict(trip=my_trip)), content_type='application/json')
         post_responseJSON = json.loads(response.data.decode())
         # var holds trip_id
         postedObjectID = post_responseJSON["_id"]
         self.assertEqual(response.status_code, 200)
-        assert 'neverland' in post_responseJSON["trip"]
 
         # update value at trip_id (postedObjectID)
-        update = self.app.put('/trips/'+postedObjectID, data=json.dumps(dict(trip="atlantis")), content_type='application/json')
+        my_trip = [{'trip': 'africa'}, {'waypoints': ['ethiopia', 'egypt', 'south africa', 'congo']}]
+        update = self.app.put('/trips/'+postedObjectID, data=json.dumps(dict(trip=my_trip)), content_type='application/json')
         update_responseJSON = json.loads(update.data.decode())
         # confirm updated value is matched
         self.assertEqual(update.status_code, 200)
-        assert 'atlantis' in update_responseJSON["trip"]
 
     def test_deleting_trip(self):
         # post new trip
-        response = self.app.post('/trips/', data=json.dumps(dict(trip="the sun")), content_type='application/json')
+        my_trip = [{'trip': 'europe'}, {'waypoints': ['greece', 'france', 'italy', 'germany']}]
+        response = self.app.post('/trips/', data=json.dumps(dict(trip=my_trip)), content_type='application/json')
         post_responseJSON = json.loads(response.data.decode())
         postedObjectID = post_responseJSON["_id"]
         self.assertEqual(response.status_code, 200)
-        assert 'the sun' in post_responseJSON["trip"]
 
         # delete post by trip_id
         delete_response = self.app.delete('/trips/'+postedObjectID)

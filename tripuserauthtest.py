3f5bf541f057ee90ec3e22d474cf7249abdefe05
kvncaldwll <kvn.caldwll@gmail.com>
2015-10-20 17:00:18 -0700

    test trip post with username

--- tripuserauthtest.py
+++ tripuserauthtest.py
@@ -18,9 +18,18 @@ class FlaskrTestCase(unittest.TestCase):
 
         # Drop collection (significantly faster than dropping entire db)
         db.drop_collection('users')
+        db.drop_collection('trips')
+
 
     def test_posting_user(self):
-        response = self.app.post('/users/', data=json.dumps(dict(username='kevin', password='secret123')), content_type='application/json')
+        response = self.app.post('/users/', data=json.dumps(dict(username='admin', password='secret')), content_type='application/json')
+        responseJSON = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        assert 'application/json' in response.content_type
+        return responseJSON
+
+    def post_with_auth(self):
+        response = self.app.post('/trips/', data=json.dumps(dict(trip='europe', waypoints=['london', 'paris', 'milan'])), content_type='application/json', header=('admin', 'secret'))
         responseJSON = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
         assert 'application/json' in response.content_type

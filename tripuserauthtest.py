cac3fc4dd3f56e51e3012da203672462d0f89331
kvncaldwll <kvn.caldwll@gmail.com>
2015-10-22 04:01:12 -0700

    more test cases

--- tripuserauthtest.py
+++ tripuserauthtest.py
@@ -27,17 +27,34 @@ class FlaskrTestCase(unittest.TestCase):
         db.drop_collection('users')
         db.drop_collection('trips')
 
-    def test_post_user(self):
+    def test_userdb(self):
         response = self.app.post('/users/',
                                 data=json.dumps(dict(username='admin',
                                 password='secret')),
                                 content_type='application/json')
         responseJSON = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
-        return responseJSON
-        # assert 'application/json' in response.content_type
+        print(responseJSON)
 
-    def test_post_trip_auth(self):
+        trip_data = dict(trip='africa', waypoints=['egypt', 'ethiopia', 'south africa'])
+        response = self.app.post('/trips/',
+                                 data=json.dumps(trip_data),
+                                 content_type='application/json',
+                                 headers=auth_header('admin', 'secret'))
+        responseJSON = json.loads(response.data.decode())
+        postedObjectID = responseJSON['_id']
+        self.assertEqual(response.status_code, 200)
+        print(responseJSON)
+
+        response = self.app.get('users/'+postedObjectID,
+                                headers=auth_header('admin', 'secret'))
+        responseJSON = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        print(responseJSON)
+
+
+    def test_tripdb_auth(self):
+        # create new user
         response = self.app.post('/users/',
                                 data=json.dumps(dict(username='admin',
                                 password='secret')),
@@ -45,7 +62,7 @@ class FlaskrTestCase(unittest.TestCase):
         responseJSON = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 200)
 
-
+        # post new trip with auth
         trip_data = dict(trip='europe', waypoints=['london', 'paris', 'milan'])
         response = self.app.post('/trips/',
                                  data=json.dumps(trip_data),
@@ -54,27 +71,49 @@ class FlaskrTestCase(unittest.TestCase):
         responseJSON = json.loads(response.data.decode())
         postedObjectID = responseJSON['_id']
         self.assertEqual(response.status_code, 200)
+        print(responseJSON)
+
+        # update existing trip with auth
+        up_data = dict(trip='europe', waypoints=['brussels', 'paris', 'amsterdam'])
+        response = self.app.put('trips/'+postedObjectID,
+                                data=json.dumps(up_data),
+                                content_type='application/json',
+                                headers=auth_header('admin', 'secret'))
+        responseJSON = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
+        print(responseJSON)
 
         # get existing trip with auth
         response = self.app.get('trips/'+postedObjectID,
                                 headers=auth_header('admin', 'secret'))
         responseJSON = json.loads(response.data.decode())
+        self.assertEqual(response.status_code, 200)
         print(responseJSON)
 
         # get non existing trip
         response = self.app.get('trips/57389d84496254540367aa0d',
                                 headers=auth_header('admin', 'secret'))
+        responseJSON = json.loads(response.data.decode())
         self.assertEqual(response.status_code, 404)
-        print(response.status_code)
-
-        # update trip with auth
-
+        print(responseJSON)
 
         # delete trip with auth
         response = self.app.delete('trips/'+postedObjectID,
                                 headers=auth_header('admin', 'secret'))
         self.assertEqual(response.status_code, 200)
-        print(response.status_code)
+        print(responseJSON)
+
+        # delete non existing trip
+        response = self.app.delete('trips/68389d84496254540367aa0d',
+                                headers=auth_header('admin', 'secret'))
+        self.assertEqual(response.status_code, 200)
+        print(responseJSON)
+
+        # get deleted trip
+        response = self.app.get('trips/'+postedObjectID,
+                                headers=auth_header('admin', 'secret'))
+        self.assertEqual(response.status_code, 404)
+        print(responseJSON)
 
 
 if __name__ == '__main__':
